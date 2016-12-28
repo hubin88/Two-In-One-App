b9ec00b62727c1e26d319baff9ba90179b11d6b5
庾景洲 <JingzhouYu@gmail.com>
2016-12-28 12:38:32 +0800

    交易首页 静态布局完成

--- app.config.js
+++ app.config.js
@@ -1,6 +1,6 @@
 const config = {
   server: {
-    host: 'localhost', // IP 地址
+    host: '192.168.1.127', // IP 地址
     port: 9000, // 端口号
   },
   copyFile: [
--- src/views/home/home.jsx
+++ src/views/home/home.jsx
@@ -17,43 +17,68 @@ export default class Home extends Component {
     };
   }
 
-  renderHome() {
+  renderProperty() {
     return (
-      <div styleName="content">
-        <div styleName="user-property">
-          <img src={require('../../images/touxiang.jpg')} alt="" />
-          <span styleName="property">总资产<b styleName="money">68.50</b>元</span>
-          <div styleName="button">
-            <span styleName="Recharge">充值</span>
-            <span styleName="withdraw">提现</span>
-          </div>
+      <div styleName="user-property">
+        <img src={require('../../images/touxiang.jpg')} alt="" />
+        <span styleName="property">总资产<b styleName="money">68.50</b>元</span>
+        <div styleName="button">
+          <img src="" alt="" />
+          <span styleName="Recharge">充值</span>
+          <span styleName="withdraw">提现</span>
         </div>
-        <div styleName="commodity-quotations">
-          <ul>
-            {this.state.quotations.map((quotations) =>
-              <li>
-                <div>
-                  <span>{quotations.name}</span>
-                  <span>{quotations.value}</span>
-                </div>
-                <div>
-                  <img src="" alt="" />
-                </div>
-              </li>
-            )}
-          </ul>
-        </div>
-        <div styleName="trend-chart">走势图</div>
-        <div styleName="building-button">下单按钮</div>
-        <div styleName="exchange-hour">交易时间</div>
       </div>
     );
   }
 
+  renderQuotations() {
+    return (
+      <div styleName="commodity-quotations">
+        <ul>
+          {this.state.quotations.map((quotations) =>
+            <li>
+              <div styleName="quotations-info">
+                <span>{quotations.name}</span>
+                <span>{quotations.value}</span>
+              </div>
+              <div>
+                <img src="" alt="" />
+              </div>
+            </li>
+          )}
+        </ul>
+      </div>
+    );
+  }
+
+  renderChart() {
+    return (
+      <div styleName="trend-chart">走势图</div>
+    );
+  }
+
+  renderBuildingButton() {
+    return (
+      <div styleName="building-button">
+        <img src="" alt="" />
+        <span styleName="bullish">看涨</span>
+        <span styleName="bearish">看跌</span>
+      </div>
+    );
+  }
+
+
   render() {
     return (
       <div styleName="home">
-        {this.renderHome()}
+        {this.renderProperty()}
+        {this.renderQuotations()}
+        {this.renderChart()}
+        {this.renderBuildingButton()}
+        <div styleName="exchange-hour">
+          <img src="" alt="" />
+          <span>交易时间:周一至周五08:00-次日04:00 每日04:30-07:00休市结算</span>
+        </div>
       </div>
     );
   }
