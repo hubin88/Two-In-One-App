71845fb1bcd86d52e477741ac3b366d798a6c3c4
hubin <764326904@qq.com>
2017-01-20 11:35:34 +0800

    添加登录注册密码包裹层

---
+++ src/views/sign-wrap/register-wrap.jsx
@@ -0,0 +1,38 @@
+/**
+ * Created by admin on 2017/1/20.
+ */
+
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { browserHistory } from 'react-router';
+import Register from '../sign/register';
+
+class RegisterWrap extends Component {
+  static propTypes = {
+    dispatch: PropTypes.func,
+    systemInfo: PropTypes.object,
+    exchangeInfo: PropTypes.object,
+  };
+  registerSuc = () => {
+    browserHistory.push('/');
+  };
+
+  render() {
+    return (
+      <Register
+        registerSuccess={this.registerSuc}
+        orgId={this.props.exchangeInfo.orgId}
+        systemType={this.props.systemInfo.systemType}
+        type="register"
+      />
+    );
+  }
+}
+function mapStateToProps(state) {
+  return {
+    systemInfo: state.systemInfo,
+    exchangeInfo: state.exchangeInfo,
+  };
+}
+
+export default connect(mapStateToProps)(RegisterWrap);
